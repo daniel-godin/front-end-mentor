40ca37049a4465bf40167b35577d00bc878ce3f9
Daniel Godin <DanielEGodin@gmail.com>
2024-09-22 07:05:06 -0700

    Improvements

--- src/pages/MortgageRepaymentCalculator.tsx
+++ src/pages/MortgageRepaymentCalculator.tsx
@@ -1,5 +1,10 @@
 import '../assets/mortgageRepaymentCalculator/mortgageRepaymentCalculator.css';
+import emptyIMG from "../assets/mortgageRepaymentCalculator/images/illustration-empty.svg";
+import favicon from "../assets/mortgageRepaymentCalculator/images/favicon-32x32.png";
+import iconCalculator from "../assets/mortgageRepaymentCalculator/images/icon-calculator.svg";
 
+
+// Functionality
 export default function MortgageRepaymentCalculator() {
 
 
@@ -26,23 +31,31 @@ function CalculatorContainer() {
 }
 
 function Calculator() {
+
+
+    const clearForm = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("TEST: clearForm function triggered")
+    }
+
     return (
         <div id='calculatorContainer'>
             <header>
-                <h2>Mortgage Calculator</h2>
-                <p>Clear All</p>
+                <h3>Mortgage Calculator</h3>
+                <p><a id='clearForm' onClick={clearForm}>Clear All</a></p>
             </header>
             
             <form id='formCalculator'>
-                <label>Mortgage Amount
-                    <input type='number'></input>
-                </label>
-                <label>Mortgage Term
-                    <input type='number'></input>
-                </label>
-                <label>Interest Rate 
-                    <input type='number'></input>
-                </label>
+                <label htmlFor='inputAmount'>Mortgage Amount</label>
+                <input type='number' id='inputAmount'></input>
+
+
+                <label htmlFor='inputTerm'>Mortgage Term</label>
+                <input type='number'id='inputTerm'></input>
+
+                <label htmlFor='inputRate'>Interest Rate</label>
+                <input type='number'id='inputRate'></input>
+
                 <fieldset>
                     <legend>Mortgage Type </legend>
 
@@ -79,9 +92,9 @@ function ResultsEmpty() {
 
     return (
         <>
-        
-        
-        
+            <img height='100px' width='100px' src={emptyIMG}></img>
+            <h3>Results shown here</h3>
+            <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
         </>
     )
 
